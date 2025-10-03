0fa7cad04c5aa492c7ace08214eaf85831df8825
Jesus Guzman, Jr. <jesusguzmanjr@icloud.com>
2025-10-03 02:40:17 +0000

    gonna test preprocessing

--- src/bin/preprocess.rs
+++ src/bin/preprocess.rs
@@ -5,9 +5,10 @@ use {
     icu_normalizer::ComposingNormalizerBorrowed,
     itertools::Itertools,
     parquet::arrow::arrow_reader::ParquetRecordBatchReaderBuilder,
+    rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator},
     regex::Regex,
     rust_gpt::utils::canonicalize_path,
-    std::{fs::File, path::PathBuf, sync::LazyLock},
+    std::{fs::File, io::Write, path::PathBuf, sync::LazyLock},
 };
 
 /// Parse a Stanford Oval Wikipedia parquet file creating shards of normalized,
@@ -26,13 +27,21 @@ struct Args {
     output_dir: PathBuf,
 }
 
+/// A Wikipedia article.
 struct Article {
+    /// The title of the article.
     document_title: String,
+
+    /// The sections of the article.
     sections: Vec<Section>,
 }
 
+/// A section of a Wikipedia article.
 struct Section {
+    /// The title of the section. This may be None.
     title: Option<String>,
+
+    /// The content of the section.
     content: String,
 }
 
@@ -49,203 +58,169 @@ fn main() -> Result<()> {
         })?;
     }
 
-    ParquetRecordBatchReaderBuilder::try_new(File::open(&canonicalize_path(&args.input_file)?)?)?
-        .with_batch_size(200)
-        .with_limit(50)
-        .build()?
-        .filter_map(|batch| {
-            if let Err(error) = &batch {
-                eprintln!("{error}");
-            }
-            batch.ok()
-        })
-        .map(|batch| {
-            // see dataset card for column names
-            // https://huggingface.co/datasets/stanford-oval/wikipedia
-            let document_title = column(&batch, "document_title")?;
-            let section_title = column(&batch, "section_title")?;
-            let content = column(&batch, "content")?;
+    // Buffer up to 64 articles that need to be further processed and sharded
+    let (tx, rx) = crossbeam_channel::bounded(64);
 
-            let rows = (0..batch.num_rows())
-                .map(|i| {
-                    let document_title = document_title.value(i);
-                    let section_title = section_title.value(i);
-                    let content = content.value(i);
-                    (document_title, section_title, content)
-                })
-                .filter(|(document_title, _, content)| {
-                    !document_title.is_empty() && !content.is_empty()
-                })
-                .map(|(document_title, section_title, content)| {
-                    (
-                        document_title,
-                        if section_title.is_empty() {
-                            None
-                        } else {
-                            Some(section_title)
-                        },
-                        content,
-                    )
-                })
-                .collect::<Vec<_>>();
+    let reader = ParquetRecordBatchReaderBuilder::try_new(File::open(&canonicalize_path(
+        &args.input_file,
+    )?)?)?
+    .with_batch_size(rayon::current_num_threads() * 16)
+    .with_limit(50)
+    .build()?;
 
-            let rows = rows
-                .iter()
-                .map(|(document_title, section_title, content)| {
-                    anyhow::Ok((
-                        process_text(document_title)?,
-                        if let Some(section_title) = section_title {
-                            Some(process_text(section_title)?)
-                        } else {
-                            None
-                        },
-                        process_text(content)?,
-                    ))
-                })
-                .filter_map(|result| {
-                    if let Err(error) = &result {
-                        eprintln!("{error}");
-                    }
-                    result.ok()
-                })
-                .collect::<Vec<_>>();
+    // Reads rows from the parquet file and produces assembled Wikipedia articles.
+    std::thread::spawn(move || {
+        reader
+            .filter_map(|batch| {
+                if let Err(error) = &batch {
+                    eprintln!("{error}");
+                }
+                batch.ok()
+            })
+            .map(|batch| {
+                // see dataset card for column names
+                // https://huggingface.co/datasets/stanford-oval/wikipedia
+                let document_title = column(&batch, "document_title")?;
+                let section_title = column(&batch, "section_title")?;
+                let content = column(&batch, "content")?;
 
-            anyhow::Ok(rows)
-        })
-        .filter_map(|result| {
-            if let Err(error) = &result {
-                eprintln!("{error}");
-            }
-            result.ok()
-        })
-        .flatten()
-        .chunk_by(
-            // Groups consecutive rows that have the same document_title
-            |(document_title, ..)| document_title.to_owned(),
-        )
-        .into_iter()
-        .map(|(document_title, group)| Article {
-            document_title,
-            sections: group
-                .map(|(_, section_title, content)| Section {
-                    title: section_title,
-                    content,
-                })
-                .collect::<Vec<_>>(),
-        })
-        .for_each(|article| {
-            println!("Article: {}", article.document_title);
-            println!("Sections: {}", article.sections.len());
+                let rows = (0..batch.num_rows())
+                    .map(|i| {
+                        let document_title = document_title.value(i);
+                        let section_title = section_title.value(i);
+                        let content = content.value(i);
+                        (document_title, section_title, content)
+                    })
+                    .filter(|(document_title, _, content)| {
+                        !document_title.is_empty() && !content.is_empty()
+                    })
+                    .map(|(document_title, section_title, content)| {
+                        (
+                            document_title,
+                            if section_title.is_empty() {
+                                None
+                            } else {
+                                Some(section_title)
+                            },
+                            content,
+                        )
+                    })
+                    .collect::<Vec<_>>();
+
+                // process the batched rows in parallel
+                let rows = rows
+                    .par_iter()
+                    .map(|(document_title, section_title, content)| {
+                        anyhow::Ok((
+                            process_text(document_title)?,
+                            if let Some(section_title) = section_title {
+                                Some(process_text(section_title)?)
+                            } else {
+                                None
+                            },
+                            process_text(content)?,
+                        ))
+                    })
+                    .filter_map(|result| {
+                        if let Err(error) = &result {
+                            eprintln!("{error}");
+                        }
+                        result.ok()
+                    })
+                    .collect::<Vec<_>>();
+
+                anyhow::Ok(rows)
+            })
+            .filter_map(|result| {
+                if let Err(error) = &result {
+                    eprintln!("{error}");
+                }
+                result.ok()
+            })
+            .flatten()
+            .chunk_by(
+                // Groups consecutive rows that have the same document_title
+                |(document_title, ..)| document_title.to_owned(),
+            )
+            .into_iter()
+            .for_each(|(document_title, group)| {
+                let article = Article {
+                    document_title,
+                    sections: group
+                        .map(|(_, section_title, content)| Section {
+                            title: section_title,
+                            content,
+                        })
+                        .collect::<Vec<_>>(),
+                };
+
+                // Block until the channel is ready to receive the article
+                tx.send(article).expect("channel is not open");
+            });
+    });
+
+    rx.into_iter()
+        .enumerate()
+        .par_bridge()
+        .map(|(i, article)| {
+            let mut file = File::create(args.output_dir.join(format!("shard-{i}.md.zstd")))?;
+
+            let mut encoder = zstd::Encoder::new(
+                &mut file,
+                // The compression level for the zstd encoder.
+                // The higher the level, the more compressed the data will be.
+                // Decompressing is same speed regardless of the level.
+                19,
+            )?
+            .auto_finish();
+
+            // Build markdown document
+            let mut markdown = String::new();
+
+            // Add document title as H1
+            markdown.push_str("# ");
+            markdown.push_str(&article.document_title);
+            markdown.push_str("\n\n");
+
+            // Add each section
             for section in &article.sections {
-                println!("  Section: {:?}", section.title);
-                println!(
-                    "  Content preview: {}...",
-                    section.content.chars().take(100).collect::<String>()
-                );
+                // Add section title as H2 if it exists and is not empty
+                if let Some(title) = &section.title
+                    && !title.is_empty()
+                {
+                    markdown.push_str("## ");
+                    markdown.push_str(title);
+                    markdown.push_str("\n\n");
+                }
+
+                // Add section content
+                markdown.push_str(&section.content);
+                markdown.push_str("\n\n");
             }
-            println!("---");
+
+            let arena = comrak::Arena::new();
+            let options = comrak::Options::default();
+            let root = comrak::parse_document(&arena, &markdown, &options);
+
+            // Format to a string
+            let mut buffer = String::new();
+            comrak::format_commonmark(root, &options, &mut buffer)?;
+
+            // Write to encoder
+            encoder.write_all(buffer.as_bytes())?;
+
+            // Add end-of-text character
+            // https://en.wikipedia.org/wiki/C0_and_C1_control_codes
+            encoder.write_all("\u{3}".as_bytes())?;
+
+            anyhow::Ok(())
+        })
+        .for_each(|result| {
+            if let Err(error) = result {
+                eprintln!("{error}");
+            }
         });
-    // let start = Instant::now();
-    // input_files
-    //     .chunks(chunk_size)
-    //     .map(ToOwned::to_owned)
-    //     .collect::<Vec<_>>()
-    //     .par_iter()
-    //     .enumerate()
-    //     .map(|(i, chunk)| {
-    //         let mut file =
-    // File::create(args.output_dir.join(format!("shard-{i}.md.zstd")))?;
 
-    //         let mut encoder = zstd::Encoder::new(
-    //             &mut file,
-    //             // The compression level for the zstd encoder.
-    //             // The higher the level, the more compressed the data will be.
-    //             // Decompressing is same speed regardless of the level.
-    //             19,
-    //         )?
-    //         .auto_finish();
-
-    //         let markdown_options = comrak::Options::default();
-
-    //         // Normalization Form C (Canonical Composition).
-    //         //
-    //         // Process:
-    //         // - Decompose everything canonically (like NFD).
-    //         // - Recompose wherever there’s a single canonical equivalent
-    // pre-composed         // character.
-    //         //
-    //         // The key is: NFC only recomposes when there exists exactly one
-    //         // pre-composed form. So it either:
-    //         // Shrinks (if multiple code points → 1 pre-composed code point).
-    //         // Leaves unchanged (if no pre-composed exists).
-    //         //
-    //         // It never expands into more code points, because that would break
-    //         // canonical equivalence.
-    //         //
-    //         // https://www.unicode.org/reports/tr15/
-    //         let nfc = icu_normalizer::ComposingNormalizerBorrowed::new_nfc();
-
-    //         chunk
-    //             .iter()
-    //             .map(std::fs::read_to_string)
-    //             .filter_map(|result| {
-    //                 if let Err(error) = &result {
-    //                     eprintln!("{error}");
-    //                 }
-    //                 result.ok()
-    //             })
-    //             .map(|mut buffer| {
-    //                 {
-    //                     // filter out files that are less than 20 lines
-    //                     if buffer.lines().count() < 20 {
-    //                         return Ok(());
-    //                     }
-
-    //                     // remove control characters
-    //                     let cleaned = buffer
-    //                         .chars()
-    //                         .filter(|c| !c.is_control())
-    //                         .collect::<String>();
-
-    //                     assert!(
-    //                         cleaned.len() <= buffer.len(),
-    //                         "expected cleaned string to contain ≤ original"
-    //                     );
-
-    //                     nfc.normalize_to(&cleaned, &mut buffer)?;
-    //                 }
-
-    //                 // parse Markdown - create a new arena for each file to avoid
-    // accumulating                 // memory
-    //                 let arena = comrak::Arena::new();
-    //                 let parsed = comrak::parse_document(&arena, &buffer,
-    // &markdown_options);
-
-    //                 comrak::format_commonmark(parsed, &markdown_options, &mut
-    // encoder)?;                 encoder.write_all("\u{3}".as_bytes())?; //
-    // end-of-text character
-
-    //                 anyhow::Ok(())
-    //             })
-    //             .for_each(|result| {
-    //                 if let Err(error) = result {
-    //                     eprintln!("{error}");
-    //                 }
-    //             });
-
-    //         anyhow::Ok(())
-    //     })
-    //     .for_each(|result| {
-    //         if let Err(error) = result {
-    //             eprintln!("{error}");
-    //         }
-    //     });
-
-    // println!(
-    //     "Processed {} markdown files in {:0.2?}",
-    //     input_files.len().separate_with_commas(),
-    //     start.elapsed()
-    // );
     Ok(())
 }
 
