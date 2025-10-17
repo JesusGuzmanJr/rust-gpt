e9a82068142a6bb3cac57adafd576597c00d7080
Jesus Guzman, Jr. <jesusguzmanjr@icloud.com>
2025-10-17 16:48:27 +0000

    rename

--- src/kernels.cu
+++ src/kernels.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 
-__global__ void add_kernel(const float *a, const float *b, float *c, int n) {
+__global__ void vec_add_kernel(const float *a, const float *b, float *c,
+                               int n) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   if (i < n)
     c[i] = a[i] + b[i];
@@ -17,7 +18,7 @@ extern "C" void vec_add(const float *a, const float *b, float *c, int n) {
 
   int block = 256;
   int grid = (n + block - 1) / block;
-  add_kernel<<<grid, block>>>(da, db, dc, n);
+  vec_add_kernel<<<grid, block>>>(da, db, dc, n);
   cudaDeviceSynchronize();
 
   cudaMemcpy(c, dc, bytes, cudaMemcpyDeviceToHost);
