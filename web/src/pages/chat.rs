22ce84f427ad5439d58960a26da67906514208f8
Jesus Guzman, Jr. <jesusguzmanjr@icloud.com>
2025-10-19 16:15:02 +0000

    fix edit icon

--- web/src/pages/chat.rs
+++ web/src/pages/chat.rs
@@ -281,12 +281,12 @@ fn render_user_message(user_message: &UserMessage, timestamp: DateTime<Utc>) -> 
                 }
                 div.message__meta.message__meta--user {
                     span.message__time { (crate::datetime::ago(&timestamp)) }
-                    button.message__edit-btn {
-                        svg.icon.icon--sm width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" {
-                            path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7";
-                            path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z";
-                        }
-                    }
+                     button.message__edit-btn {
+                         svg.icon.icon--sm width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" {
+                             path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z";
+                             path d="m15 5 4 4";
+                         }
+                     }
                 }
             }
         }
