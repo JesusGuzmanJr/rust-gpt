c3645fa0dff74195228b8c185f637857e07bc76a
Jesus Guzman, Jr. <jesusguzmanjr@icloud.com>
2025-10-30 06:10:16 +0000

    add add tls

--- web/src/config.rs
+++ web/src/config.rs
@@ -1,14 +1,9 @@
 use {
     crate::{hash::HashKey, mailer::MailerConfig},
     anyhow::{Context, Result},
-    serde::{Deserialize, Deserializer},
-    std::{
-        io::BufReader,
-        path::{Path, PathBuf},
-    },
+    serde::Deserialize,
+    std::path::PathBuf,
 };
-
-pub(crate) type TlsConfig = rustls::ServerConfig;
 
 /// The root configuration.
 #[derive(Debug, Deserialize)]
@@ -26,8 +21,17 @@ pub(crate) struct AppConfig {
     ///
     /// If provided, listens on port 443 with TLS and redirects port http
     /// requests on port 80 to 443.
-    #[serde(deserialize_with = "parse_tls_config")]
-    pub(crate) tls: Option<TlsConfig>,
+    pub(crate) tls: Option<TlsCertPaths>,
+}
+
+/// The paths to the tls certificate and private key.
+#[derive(Debug, Deserialize)]
+pub(crate) struct TlsCertPaths {
+    /// The filepath to the tls certificate.
+    pub(crate) cert: PathBuf,
+
+    /// The filepath to the tls private key.
+    pub(crate) key: PathBuf,
 }
 
 impl AppConfig {
@@ -41,41 +45,4 @@ impl AppConfig {
         )
         .with_context(|| format!("failed to parse {path:?}"))
     }
-}
-
-/// The paths to the tls certificate and private key.
-#[derive(Deserialize)]
-struct TlsCertPaths {
-    /// The filepath to the tls certificate.
-    cert: PathBuf,
-
-    /// The filepath to the tls private key.
-    cert_key: PathBuf,
-}
-
-fn parse_tls_config<'de, D>(deserializer: D) -> Result<Option<TlsConfig>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let tls: Option<TlsCertPaths> = Deserialize::deserialize(deserializer)?;
-    tls.map(|tls| create_tls(&tls.cert, &tls.cert_key).map_err(serde::de::Error::custom))
-        .transpose()
-}
-
-fn create_tls(cert: &Path, cert_key: &Path) -> Result<TlsConfig> {
-    let open = |path| std::fs::File::open(path).with_context(|| format!("error opening {path:?}"));
-    let cert_chain = rustls_pemfile::certs(&mut BufReader::new(open(cert)?))
-        .filter_map(|cert| cert.ok())
-        .collect();
-
-    let key = rustls_pemfile::pkcs8_private_keys(&mut BufReader::new(open(cert_key)?))
-        .filter_map(|key| key.ok())
-        .next()
-        .context("no cert key found")?
-        .into();
-
-    TlsConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(cert_chain, key)
-        .context("couldn't create certificate chain")
 }
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,13 +1,18 @@
 use {
     anyhow::{Context, Result},
     axum::{
-        Router,
-        http::{HeaderName, HeaderValue, header},
+        BoxError, Router,
+        handler::HandlerWithoutStateExt,
+        http::{HeaderName, HeaderValue, Uri, header, uri::Authority},
         response::{IntoResponse, Redirect, Response},
         routing::get,
     },
+    axum_extra::extract::Host,
     bytesize::ByteSize,
-    std::thread,
+    std::{
+        net::{IpAddr, Ipv4Addr, SocketAddr},
+        thread,
+    },
     tower::service_fn,
     tower_http::{services::ServeDir, set_header::SetResponseHeaderLayer},
     tracing::*,
@@ -36,9 +41,17 @@ const PROJECT_NAME: &str = "rust-gpt";
 const PROJECT_URL: &str = "https://rust-gpt.marzipanclub.com";
 const TEAM_EMAIL: &str = "hello@marzipanclub.com";
 
+const HTTP_PORT: u16 = 3000;
+const SRC_HTTP_PORT_REDIRECT: u16 = 3080;
+const HTTPS_PORT: u16 = 3443;
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
+
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("failed to install rustls crypto provider");
 
     let config = config::AppConfig::from_config_path()?;
 
@@ -46,78 +59,142 @@ async fn main() -> Result<()> {
     persistence::init(&config.db_path)?;
     mailer::init(config.mailer).await?;
 
-    let bind_address = "127.0.0.1:3000";
-    let listener = tokio::net::TcpListener::bind(bind_address).await?;
+    let app = Router::new()
+        .route("/", get(|| async { Redirect::to(pages::chat::PATH) }))
+        .route(pages::chat::PATH, get(pages::chat::page))
+        .route(pages::privacy::PATH, get(pages::privacy::page))
+        .route(pages::signin::PATH, get(pages::signin::page))
+        .route(pages::signup::PATH, get(pages::signup::page))
+        .route(pages::terms::PATH, get(pages::terms::page))
+        .route(
+            "/style.css",
+            get((
+                [
+                    (
+                        header::CONTENT_TYPE,
+                        HeaderValue::from_static("text/css; charset=utf-8"),
+                    ),
+                    http::CACHE_PUBLICLY_15_MIN,
+                ],
+                include_bytes!(concat!(env!("OUT_DIR"), "/style.css")),
+            )),
+        )
+        .nest(
+            "/api",
+            Router::new()
+                .merge(pages::chat::api())
+                .merge(pages::signin::api())
+                .merge(pages::signup::api()),
+        )
+        .fallback_service(
+            ServeDir::new(concat!(env!("CARGO_MANIFEST_DIR"), "/assets")).fallback(service_fn(
+                |_| async move {
+                    Ok::<Response, std::convert::Infallible>(
+                        pages::not_found::page().into_response(),
+                    )
+                },
+            )),
+        )
+        .layer(SetResponseHeaderLayer::if_not_present(
+            header::X_CONTENT_TYPE_OPTIONS,
+            HeaderValue::from_static("nosniff"),
+        ))
+        .layer(axum_htmx::AutoVaryLayer)
+        .layer(axum::extract::DefaultBodyLimit::max(
+            MAX_REQUEST_BODY_SIZE.as_u64() as _,
+        ))
+        .layer(tower_governor::GovernorLayer::new({
+            // Allow bursts with up to ten requests per IP address
+            // and replenishes one element every two seconds
+            let governor_config = tower_governor::governor::GovernorConfigBuilder::default()
+                .per_second(3)
+                .burst_size(10)
+                .finish()
+                .context("invalid rate limiting governor configuration")?;
 
-    info!(%bind_address, "listening");
-    axum::serve(
-        listener,
-        Router::new()
-            .route("/", get(|| async { Redirect::to(pages::chat::PATH) }))
-            .route(pages::chat::PATH, get(pages::chat::page))
-            .route(pages::privacy::PATH, get(pages::privacy::page))
-            .route(pages::signin::PATH, get(pages::signin::page))
-            .route(pages::signup::PATH, get(pages::signup::page))
-            .route(pages::terms::PATH, get(pages::terms::page))
-            .route(
-                "/style.css",
-                get((
-                    [
-                        (
-                            header::CONTENT_TYPE,
-                            HeaderValue::from_static("text/css; charset=utf-8"),
-                        ),
-                        http::CACHE_PUBLICLY_15_MIN,
-                    ],
-                    include_bytes!(concat!(env!("OUT_DIR"), "/style.css")),
-                )),
-            )
-            .nest(
-                "/api",
-                Router::new()
-                    .merge(pages::chat::api())
-                    .merge(pages::signin::api())
-                    .merge(pages::signup::api()),
-            )
-            .fallback_service(
-                ServeDir::new(concat!(env!("CARGO_MANIFEST_DIR"), "/assets")).fallback(service_fn(
-                    |_| async move {
-                        Ok::<Response, std::convert::Infallible>(
-                            pages::not_found::page().into_response(),
-                        )
-                    },
-                )),
-            )
-            .layer(SetResponseHeaderLayer::if_not_present(
-                header::X_CONTENT_TYPE_OPTIONS,
-                HeaderValue::from_static("nosniff"),
-            ))
-            .layer(axum_htmx::AutoVaryLayer)
-            .layer(axum::extract::DefaultBodyLimit::max(
-                MAX_REQUEST_BODY_SIZE.as_u64() as _,
-            ))
-            .layer(tower_governor::GovernorLayer::new({
-                // Allow bursts with up to ten requests per IP address
-                // and replenishes one element every two seconds
-                let governor_config = tower_governor::governor::GovernorConfigBuilder::default()
-                    .per_second(3)
-                    .burst_size(10)
-                    .finish()
-                    .context("invalid rate limiting governor configuration")?;
+            let governor_limiter = governor_config.limiter().clone();
 
-                let governor_limiter = governor_config.limiter().clone();
+            thread::spawn(move || {
+                loop {
+                    thread::sleep(std::time::Duration::from_secs(20));
+                    governor_limiter.retain_recent();
+                }
+            });
 
-                thread::spawn(move || {
-                    loop {
-                        thread::sleep(std::time::Duration::from_secs(20));
-                        governor_limiter.retain_recent();
-                    }
-                });
+            governor_config
+        }));
 
-                governor_config
-            }))
-            .into_make_service_with_connect_info::<std::net::SocketAddr>(),
-    )
-    .await?;
+    let bind_address = if config.tls.is_some() {
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), HTTPS_PORT)
+    } else {
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), HTTP_PORT)
+    };
+    info!(%bind_address);
+
+    if let Some(tls) = config.tls {
+        let config = axum_server::tls_rustls::RustlsConfig::from_pem_file(tls.cert, tls.key)
+            .await
+            .context("failed to parse tls cert and key")?;
+
+        tokio::spawn(async move {
+            if let Err(error) = redirect_http_to_https().await {
+                error!(%error, "failed to redirect HTTP to HTTPS");
+            }
+        });
+
+        axum_server::bind_rustls(bind_address, config)
+            .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+            .await?;
+    } else {
+        axum_server::bind(bind_address)
+            .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+            .await?;
+    };
+
+    Ok(())
+}
+
+async fn redirect_http_to_https() -> Result<()> {
+    fn make_https(host: &str, uri: Uri) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
+
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse()?);
+        }
+
+        let authority = host.parse::<Authority>()?;
+        let bare_host = match authority.port() {
+            Some(port_struct) => authority
+                .as_str()
+                .strip_suffix(port_struct.as_str())
+                .context("suffix not found")?
+                .strip_suffix(':')
+                .context("colon not found")?,
+            None => authority.as_str(),
+        };
+
+        parts.authority = Some(format!("{bare_host}:443").parse()?);
+
+        Ok(Uri::from_parts(parts)?)
+    }
+
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(&host, uri) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(error) => {
+                tracing::warn!(%error, "failed to convert URI to HTTPS");
+                Err(axum::http::StatusCode::BAD_REQUEST)
+            }
+        }
+    };
+
+    let bind_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), SRC_HTTP_PORT_REDIRECT);
+    info!(%bind_address);
+    axum_server::bind(bind_address)
+        .serve(redirect.into_make_service())
+        .await?;
+
     Ok(())
 }
